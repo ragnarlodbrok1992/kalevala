c2b64e900c6647d06f17432c7af6bf6059a90c7a
ragnarlodbrok1992 <maraus92@gmail.com>
2023-04-15 21:24:44 +0200

    WIP

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,12 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    std.debug.print("Hello, kalevala here!\n", .{});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    // 1. Get arguments from cmdline.
+    // 2. Open file - write to file - close the file.
+    const heap_allocator = std.heap.page_allocator;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    var args = std.process.argsWithAllocator(heap_allocator);
+    std.debug.print("{any}\n", .{args});
 }
